6f96be46edde85da7ff9fa8c5465ac4b2e3abab7
NicoLiutong <1299924752@qq.com>
2017-05-17 20:25:44 +0800

    second do

--- app/src/main/java/com/example/bianqian/activity/MainActivity.java
+++ app/src/main/java/com/example/bianqian/activity/MainActivity.java
@@ -1,13 +1,18 @@
-package com.example.bianqian;
+package com.example.bianqian.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.bianqian.R;
+
+import cn.bmob.v3.Bmob;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Bmob.initialize(this,"3da138f19a8d8a32a5a64ac1c4e740df");
         setContentView(R.layout.activity_main);
     }
 }
