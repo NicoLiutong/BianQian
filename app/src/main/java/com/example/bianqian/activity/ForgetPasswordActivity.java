ed3d4be19f9741f8596f183d43f489e063aa8195
NicoLiutong <1299924752@qq.com>
2017-05-20 11:37:57 +0800

    2017.5.20

---
+++ app/src/main/java/com/example/bianqian/activity/ForgetPasswordActivity.java
@@ -0,0 +1,26 @@
+package com.example.bianqian.activity;
+
+import com.example.bianqian.R;
+
+public class ForgetPasswordActivity extends BasicActivity{
+
+    @Override
+    public void setContentView() {
+        setContentView(R.layout.activity_forget_password);
+    }
+
+    @Override
+    public void initViews() {
+
+    }
+
+    @Override
+    public void initListeners() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+}
---
+++ app/src/main/java/com/example/bianqian/activity/RegisterActivity.java
@@ -0,0 +1,26 @@
+package com.example.bianqian.activity;
+
+import com.example.bianqian.R;
+
+public class RegisterActivity extends BasicActivity {
+
+    @Override
+    public void setContentView() {
+        setContentView(R.layout.activity_register);
+    }
+
+    @Override
+    public void initViews() {
+
+    }
+
+    @Override
+    public void initListeners() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+}
